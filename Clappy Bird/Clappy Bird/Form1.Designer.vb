bf0a2a2cc68590123a2f9becb078f779381d61cf
Nicholas <36908791+exwundee@users.noreply.github.com>
2020-01-22 10:19:40 -0500

    Add files via upload

---
+++ Clappy Bird/Clappy Bird/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Bird = New System.Windows.Forms.PictureBox()
+        Me.topPipe = New System.Windows.Forms.PictureBox()
+        Me.pipe = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.Bird, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.topPipe, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pipe, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Bird
+        '
+        Me.Bird.Image = Global.Clappy_Bird.My.Resources.Resources.birb
+        Me.Bird.Location = New System.Drawing.Point(38, 94)
+        Me.Bird.Name = "Bird"
+        Me.Bird.Size = New System.Drawing.Size(75, 74)
+        Me.Bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Bird.TabIndex = 0
+        Me.Bird.TabStop = False
+        '
+        'topPipe
+        '
+        Me.topPipe.Location = New System.Drawing.Point(488, -1)
+        Me.topPipe.Name = "topPipe"
+        Me.topPipe.Size = New System.Drawing.Size(198, 114)
+        Me.topPipe.TabIndex = 1
+        Me.topPipe.TabStop = False
+        '
+        'pipe
+        '
+        Me.pipe.Location = New System.Drawing.Point(501, 337)
+        Me.pipe.Name = "pipe"
+        Me.pipe.Size = New System.Drawing.Size(198, 114)
+        Me.pipe.TabIndex = 2
+        Me.pipe.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.pipe)
+        Me.Controls.Add(Me.topPipe)
+        Me.Controls.Add(Me.Bird)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Bird, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.topPipe, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pipe, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Bird As PictureBox
+    Friend WithEvents topPipe As PictureBox
+    Friend WithEvents pipe As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Clappy Bird/Clappy Bird/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+
+    Dim yspeed As Integer = 0
+    Dim gravity As Integer = 2
+    Dim gapBetweenpipes As Integer = 440
+    Dim pipespeed As Single = 3.5
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        Timer1.Enabled = True
+        CreatePipes(1)
+        CreateTopPipes(1)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        yspeed += gravity
+        Bird.Top += yspeed
+        For i = 0 To 1
+            pipe.Left -= 2
+            topPipe.Left -= 2
+            If Collision(topPipe, Bird) = False Then
+                Application.Exit()
+            End If
+            If Collision(pipe, Bird) = False Then
+                Application.Exit()
+            
+            ElseIf pipe.Left < 0 Then
+                pipe.Left += 400
+                topPipe.Left += 400
+                pipe.Top = 70 + 290 * Rnd()
+                topPipe.Top = pipe.Top - 400
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Space Then
+            yspeed = -15
+        End If
+    End Sub
+
+    Private Sub CreatePipes(ByVal Number As Integer)
+        Dim i As Integer = 0
+        For i = 0 To Number
+            Dim teep As New PictureBox
+            Me.Controls.Add(teep)
+            teep.Width = 40
+            teep.Height = 200
+            teep.BorderStyle = BorderStyle.FixedSingle
+            teep.BackColor = Color.Green
+            teep.Top = 70 + 290 * Rnd()
+            teep.Left = (i * 200) + 350
+            pipe = teep
+            pipe.Visible = True
+        Next
+    End Sub
+
+    Private Sub CreateTopPipes(ByVal Number As Integer)
+        Dim i As Integer = 0
+        For i = 0 To Number
+
+        Next
+        Dim teep As New PictureBox
+        Me.Controls.Add(teep)
+        teep.Width = 40
+        teep.Height = 300
+        teep.BorderStyle = BorderStyle.FixedSingle
+        teep.BackColor = Color.Green
+        teep.Top = (i * 200) + Rnd()
+        teep.Left = (i * 200) + 100
+        topPipe = teep
+        topPipe.Visible = True
+    End Sub
+
+    Private Function Collision(ByVal Object1 As Object, ByVal Object2 As Object) As Boolean
+        Dim Collided As Boolean = False
+        If Object1.top + Object1.Height >= Object2.top And
+                Object1.top + Object1.Height >= Object2.top And
+                Object1.left + Object1.Width >= Object2.Left And
+                Object1.left + Object1.Width >= Object2.Left And Object1.Visible = True And Object1.Visible = True Then
+            Collided = True
+        End If
+        Return Collided
+    End Function
+
+End Class
